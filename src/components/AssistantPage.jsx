a99390e6450a304f36c6ca61cbb4d83e91194015
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-10-02 14:16:35 +0100

    avatar

--- src/components/AssistantPage.jsx
+++ src/components/AssistantPage.jsx
@@ -1,25 +1,31 @@
 let timeoutId;
 const buttonExplanations = {
-    'button1': 'Home button was clicked. This button can perform will take you to the home page.',
-    'button2': 'About button was clicked. This button will tell you about the project.',
-    'button3': 'Contact button was clicked. This button will provide contact information of the project Author.',
-    'button4': 'Explore button was clicked. This button can navigate to other sections.',
+    'button1': 'Sign Up button was clicked. This button can perform a sign-up action.',
+    'button2': 'Login button was clicked. This button can perform a login action.',
+    'button3': 'Learn More button was clicked. This button can provide more information.',
+    'button4': 'Contact Us button was clicked. This button can open a contact form.',
+    'button5': 'Explore button was clicked. This button can navigate to other sections.',
+    'button6': 'Get Started button was clicked. This button can initiate the onboarding process.'
 };
 
 const videoContainer = document.getElementById('video-container');
 const explanationElm = document.getElementById('explanation');
 
 
-
+// Function to reset the video container and clear explanation text
 function resetVideoContainer() {
-  
+    // Return video container to its normal position
     videoContainer.style.top = '80%';
     videoContainer.style.left = '80%';
 
+    // Clear the explanation text
     explanationElm.textContent = '';
+
+    // Remove the "active" class to hide the video container
     videoContainer.classList.remove('active');
 }
 
+// Add click event listeners to each button
 Object.keys(buttonExplanations).forEach(buttonId => {
     const button = document.getElementById(buttonId);
     button.addEventListener('click', function () {
@@ -34,9 +40,13 @@ Object.keys(buttonExplanations).forEach(buttonId => {
             height: rect.height,
         };
 
+        // Move the video container close to the clicked button
         videoContainer.style.top = `${buttonPosition.top + buttonPosition.height + 10}px`;
         videoContainer.style.left = `${buttonPosition.left + buttonPosition.width / 2 + 10}px`;
 
+
+
+        // Show the video container by adding the "active" class
         videoContainer.classList.add('active');
 
         if (videoContainer.classList.contains('active')) {
@@ -45,6 +55,7 @@ Object.keys(buttonExplanations).forEach(buttonId => {
 
         clearTimeout(timeoutId);
 
+        // Set a new timer to reset the video container and clear the text after 1 minute
         timeoutId = setTimeout(resetVideoContainer, 5000);
 
     });
